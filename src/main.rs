c0590147fb06484a28144586482d26843f6aebe9
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-02-20 19:19:26 -0300

    mod

--- src/main.rs
+++ src/main.rs
@@ -12,3 +12,42 @@ struct Pendulum{
     m: f32, // Ball mass
     g: f32, // The gravity
 }
+
+impl Pendulum {
+    fn new(){
+
+    }
+
+    fn update(){
+
+    }
+
+    fn draw() {
+
+    }
+}
+
+mod vector { // isolate sequencitial code
+
+    struct Vector{
+        pub x: f32,
+        pub y: f32,
+    }
+
+    impl Vector {
+        pub fn new(x: f32, y: f32) -> Vector{
+            Vector { 
+                x, 
+                y,
+            }
+        }
+
+        pub fn add(){
+
+        }
+
+        pub fn set(){
+
+        }
+    }
+}
